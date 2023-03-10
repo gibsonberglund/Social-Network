09ae1307ee6d4c1c015190b9385e9e6af9bbd03e
Your Name <berglundgibson@gmail.com>
2023-03-10 15:04:51 -0300

    added controllers and utils

---
+++ Progress.md
@@ -0,0 +1,22 @@
+# Track the challenge progress
+
+## DONE
+
+# Main index.js
+
+# Models folder
+
+# Routes folder
+    -routes for viewing friends currently commented out
+
+# Config folder?
+
+# Controllers
+
+# Utils
+
+
+
+## TO DO
+
+# Testing with Insomnia
---
+++ utils/data.js
@@ -0,0 +1,118 @@
+const names = [
+  'Aaran',
+  'Aaren',
+  'Aarez',
+  'Aarman',
+  'Aaron',
+  'Aaron-James',
+  'Aarron',
+  'Aaryan',
+  'Aaryn',
+  'Aayan',
+  'Aazaan',
+  'Abaan',
+  'Abbas',
+  'Abdallah',
+  'Abdalroof',
+  'Abdihakim',
+  'Abdirahman',
+  'Abdisalam',
+  'Abdul',
+  'Abdul-Aziz',
+  'Abdulbasir',
+  'Abdulkadir',
+  'Abdulkarem',
+  'Smith',
+  'Jones',
+  'Coollastname',
+  'enter_name_here',
+  'Ze',
+  'Zechariah',
+  'Zeek',
+  'Zeeshan',
+  'Zeid',
+  'Zein',
+  'Zen',
+  'Zendel',
+  'Zenith',
+  'Zennon',
+  'Zeph',
+  'Zerah',
+  'Zhen',
+  'Zhi',
+  'Zhong',
+  'Zhuo',
+  'Zi',
+  'Zidane',
+  'Zijie',
+  'Zinedine',
+  'Zion',
+  'Zishan',
+  'Ziya',
+  'Ziyaan',
+  'Zohaib',
+  'Zohair',
+  'Zoubaeir',
+  'Zubair',
+  'Zubayr',
+  'Zuriel',
+  'Xander',
+  'Jared',
+  'Courtney',
+  'Gillian',
+  'Clark',
+  'Jared',
+  'Grace',
+  'Kelsey',
+  'Tamar',
+  'Alex',
+  'Mark',
+  'Tamar',
+  'Farish',
+  'Sarah',
+  'Nathaniel',
+  'Parker',
+];
+
+const thoughts = [
+  'Why is it called Shampoo',
+  'Otters are bastards',
+  'Learn Piano',
+  'How to survive in space',
+  'Castles made of sand',
+  'Monopoly Money Manager',
+  'I watched every movie ever',
+  'This is what my cat thinks of me',
+  'Reddit melted my brain',
+  'History you will not learn in school',
+  'Acting is just good pretending',
+  'How does the internet work',
+  'Magnets, how do they work',
+  'A computer wrote my essay',
+  'Is typing enough exercise',
+  'How to make the easiest dinner',
+  'I lost a million dollars in one hand of poker',
+  'How to prevent Skynet',
+];
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets a random full name
+const getRandomName = () =>
+  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+// Function to generate random assignments that we can add to student object.
+const getRandomThoughts = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughtName: getRandomArrItem(thoughts),
+      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+    });
+  }
+  return results;
+};
+
+// Export the functions for use in seed.js
+module.exports = { getRandomName, getRandomThoughts };
